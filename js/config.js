d1feac1cb4bbf7f97da974e2913f7060b58ceae0
MariaNazarenaGonzalez <101027506+MariaNazarenaGonzalez@users.noreply.github.com>
2026-06-21 17:54:56 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-    SHEET_ID: "",
-    SHEET_NAME: "",
+    SHEET_ID: "1IkjsfSoToJTMG5e9TgMUSOBmNjWoX-024dnYCQWRa5o",
+    SHEET_NAME: "Catalogo",
     WHATSAPP_NUMBER: "",
     CURRENCY: "$"
 };
