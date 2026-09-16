6f5353c290147759b20de13193b80373a94875fd
Naza <nazarena13gonzalez@gmail.com>
2026-09-16 19:39:02 -0300

    mejoras

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     SHEET_ID: "1e8owrFM2ckaCi2008E1EOu4WC7y6k_kscxbTJf7GmkM",
     SHEET_NAME: "Productos",
-    WHATSAPP_NUMBER: "5492325564405",
+    WHATSAPP_NUMBER: "5492325564409",
     CURRENCY: "$"
 };
